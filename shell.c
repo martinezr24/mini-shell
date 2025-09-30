237810a9702aa862ce202e90f8874e89cb8913b4
Gio Martinez <giovannimart22@gmail.com>
2025-09-30 11:33:06 -0400

    Handled cd, and other built in commands

--- shell.c
+++ shell.c
@@ -17,24 +17,59 @@ void print_prompt() {
 int main(int argc, char** argv){
   alarm(120); // set a timer for 120 seconds to prevent accidental infinite loops or fork bombs
 
+  // buffer to store user input
   char input[BUFFER_SIZE];
 
-  // infinite loop to read input and print back what was typed
   while (1) {
     print_prompt();
 
+    // read input from user
     if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
-      printf("\n");
+      printf("\n"); 
       break;
     }
 
     input[strcspn(input, "\n")] = '\0';
-
     if (strcmp(input, "exit") == 0) break;
-  
     if (strlen(input) == 0) continue;
 
-    printf("You typed: %s\n", input);
+    char *args[BUFFER_SIZE];
+    int i = 0;
+    char *token = strtok(input, " ");
+
+    while (token != NULL) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    // handle built-in cd command
+    if (strcmp(args[0], "cd") == 0) {
+      char *target_dir = args[1];
+
+      if (target_dir == NULL) {
+        target_dir = getenv("HOME");
+        if (target_dir == NULL) {
+            fprintf(stderr, "couldn't find a home directory\n");
+            continue;
+        }
+      }
+      if (chdir(target_dir) != 0) fprintf(stderr, "cd failed\n");
+
+      continue;
+    }
+
+    // create a new process to execute the command
+    pid_t pid = fork();
+    if (pid == 0) {
+        execvp(args[0], args);
+        fprintf(stderr, "exec failed\n");
+        exit(1);
+    } else if (pid > 0) {
+        wait(NULL);
+    } else {
+      fprintf(stderr, "fork failed\n");
+    }
   }
  
   return 0;
